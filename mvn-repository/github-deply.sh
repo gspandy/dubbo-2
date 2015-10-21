9f5e52271944905a4e2b4d8e366ea1a6b0d3bf84
lpn520 <lpn520@gmail.com>
2015-10-21 16:15:36 +0800

    Merge branch dubbo-2.4.x

---
+++ mvn-repository/github-deply.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ../dubbo/
+mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+
+cd ../dubbo-demo-dubbo-demo-provider/
+mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+
+cd ../dubbo-demo-dubbo-demo-consumer/
+mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
