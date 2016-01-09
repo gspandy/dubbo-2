b8665887b0303ef757bb50364044ba6148ffc87d
钩吻 <pinian.lpn@alibaba-inc.com>
2016-01-09 10:29:59 +0800

    Merge branch 'dubbo-2.4.x'

--- dubbo-demo/dubbo-demo-consumer/src/com/alibaba/dubbo/demo/consumer/web/Index.java
+++ dubbo-demo/dubbo-demo-consumer/src/com/alibaba/dubbo/demo/consumer/web/Index.java
@@ -1,19 +1,21 @@
 package com.alibaba.dubbo.demo.consumer.web;
 
 import com.alibaba.dubbo.demo.provider.service.DemoService;
+import com.github.jsplite.http.JspliteHttpRequest;
+import com.github.jsplite.http.JspliteHttpResponse;
+import com.github.jsplite.mvc.Controller;
+import com.github.jsplite.mvc.ModelView;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+public class Index implements Controller {
 
-public class Index {
+    private ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring-*.xml");
 
-    private static ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring-*.xml");
-
-    public static void execute(HttpServletRequest request,
-                        HttpServletResponse response) throws Exception {
-        DemoService demoService = (DemoService)ctx.getBean("demoService");
+    @Override
+    public void execute(JspliteHttpRequest request, JspliteHttpResponse response,
+                        ModelView model) throws Exception {
+        DemoService demoService = (DemoService) ctx.getBean("demoService");
         String value = demoService.sayHello("钩吻");
         request.setAttribute("hello", value);
     }
